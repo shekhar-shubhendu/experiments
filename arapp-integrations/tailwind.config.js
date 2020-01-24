eea17fbd848ee49e5e09433717065e7d44678b95
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2020-01-24 09:43:09 +0100

    initial + tailwind setup

---
+++ arapp-integrations/tailwind.config.js
@@ -0,0 +1,35 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif']
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px'
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff'
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem'
+      }
+    }
+  }
+}
